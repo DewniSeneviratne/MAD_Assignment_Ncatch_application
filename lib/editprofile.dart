6169380d4941bc66c9372f59ef3d4884dce19a00
Harindi Balasuriya <96609085+Harindi21@users.noreply.github.com>
2023-09-18 14:11:58 +0530

    Update editprofile.dart
    Added ne w code to profile page

--- lib/editprofile.dart
+++ lib/editprofile.dart
@@ -1,47 +1,184 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _HomeState extends State<Home> {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool showPw = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(150),
-        child: Stack(
-          children: [
-            AppBar(
-              backgroundColor: Colors.pink,
-              elevation: 0.0,
-              title: const Text("Hi Zara"),
-              leading: IconButton(
-                icon: const Icon(Icons.menu),
-                onPressed: () {},
-              ),
-            ),
-            Positioned(
-              top: 160.0, // Adjust the top position as needed
-              right: 10.0, // Adjust the right position as needed
-              child: ClipOval(
-                child: Image.asset(
-                  'assets/propic.jpg',
-                  width: 60, // Adjust the size as needed
-                  height: 60, // Adjust the size as needed
-                  fit: BoxFit.cover,
-                ),
-              ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {},
             ),
           ],
+          title: const Text("Hi Zara"),
+        ),
+
+        //The outer Container is used to apply padding to the content inside the ListView
+        body: Container(
+          padding: const EdgeInsets.only(left: 16, top: 22, right: 16),
+          child: GestureDetector(
+            //provides a gesture recognition framework
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: ListView(
+              children: [
+                const Text(
+                  "edit profile",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ), //SizedBox is to add spacing (vertical space) between the "edit profile"
+                Center(
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        width: 130,
+                        height: 130,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 4,
+                              color: Theme.of(context).scaffoldBackgroundColor),
+                          boxShadow: [
+                            BoxShadow(
+                                spreadRadius: 7,
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.1),
+                                offset: const Offset(0.10, 0.1))
+                          ],
+                          shape: BoxShape.circle,
+                          image: const DecorationImage(
+                            image: AssetImage(
+                              'assets/propic.jpg',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: const BoxDecoration(
+                            color: Colors.green,
+                          ),
+                          child: const Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 36,
+                ),
+                textFieldMethod("Name", "Harindi", false),
+                textFieldMethod("Email", "eranthikah999@gmail.com", false),
+                textFieldMethod("Password", "AAAAAAAAA", true),
+                textFieldMethod("Location", "Colombo", false),
+                const SizedBox(
+                  height: 35,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 49),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Cancel",
+                        style: TextStyle(
+                            fontSize: 14,
+                            letterSpacing: 2.2,
+                            color: Colors.black),
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          padding: const EdgeInsets.symmetric(horizontal: 49),
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        ),
+                        child: const Text(
+                          "SAVE",
+                          style: TextStyle(
+                              fontSize: 11,
+                              letterSpacing: 2.2,
+                              color: Colors.white),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
-      body: Container(
-        // Your main content goes here
-        color: Colors.white, // Set the background color of the body
+    );
+  }
+
+  //TextField textFieldMethod(String labelText, String placeholder) {
+  Widget textFieldMethod(
+      String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        obscureText: isPasswordTextField ? showPw : false,
+        decoration: InputDecoration(
+          suffixIcon: isPasswordTextField
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      showPw = !showPw;
+                    });
+                  },
+                  icon: const Icon(Icons.remove_red_eye, color: Colors.grey),
+                )
+              : null,
+          contentPadding: const EdgeInsets.only(bottom: 3),
+          labelText: labelText,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder,
+          hintStyle: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
       ),
     );
   }
