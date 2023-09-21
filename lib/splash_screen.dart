f4d44ba358ef5df777e88511684684931fbcdb1f
dilantharaka22759 <dilanalwis35@gmail.com>
2023-09-21 06:51:56 +0530

    Update splash_screen.dart
    Import packages and create class

---
+++ lib/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+
+import 'package:flutter/material.dart';
+import 'package:ncatch_app/home_screen.dart';
+
+class SplashScreen extends StatefulWidget{
+  const SplashScreen ({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+
+}
