3de46435cbb4b60f7acbb715c44c36b8298b32f4
dilantharaka22759 <dilanalwis35@gmail.com>
2023-09-21 06:54:28 +0530

    Update splash_screen.dart
    Finalize splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -11,3 +11,46 @@ class SplashScreen extends StatefulWidget{
   State<SplashScreen> createState() => _SplashScreenState();
 
 }
+
+class _SplashScreenState extends State<SplashScreen>{
+
+ @override
+void initState() {
+  super.initState();
+  Timer(Duration(seconds: 6),
+  ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(),))
+  );
+  
+}
+
+  @override
+  Widget build(BuildContext context){
+    return Material(
+      child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image:AssetImage("images/Back01.png"),
+            fit: BoxFit.cover,
+            
+
+             ),
+        ),
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset('images/N Catch.png',
+            height: 250.0,
+            width: 250.0,
+            ),
+            CircularProgressIndicator(),
+          ],
+        )
+      
+    ),
+    );
+  }
+}
